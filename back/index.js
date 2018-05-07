1b883350ea2f034e9682a5dc088764ed99a3e8e5
Your SouadAli10 <Souad.Ali10@gmail.com>
2018-05-07 12:50:48 +0300

    first commit

---
+++ back/index.js
@@ -0,0 +1,35 @@
+const http = require('http')
+
+const server = http.createServer()
+
+const handleRequest = (req, res) => {
+  res.end('ok!')
+}
+
+server.on('request', handleRequest)
+server.listen(8888, ()=> console.log(`server is ready`))
+const io = require('socket.io')(server);
+
+let globalNumber = 0
+
+io.on('connection', (socket) => {
+  
+  console.log('a user connected')
+	
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+	
+  socket.on('increment', () => {
+    globalNumber++
+    io.emit('number:change',globalNumber)
+  });
+
+  socket.on('decrement', () => {
+    globalNumber--
+    io.emit('number:change',globalNumber)
+  });
+
+  socket.emit('number:change',globalNumber)
+
+});
