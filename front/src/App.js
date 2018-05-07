0a05a9f2583266faaea9702380fd8ad855b32858
Your SouadAli10 <Souad.Ali10@gmail.com>
2018-05-07 13:29:17 +0300

    solved counter

--- front/src/App.js
+++ front/src/App.js
@@ -2,32 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import io from 'socket.io-client'
-class App extends Component{
-  
-    state = { socket:null, globalNumber:0 }
-  
-    componentDidMount(){
-      const socket = io('http://localhost:8888');
-  
-      this.setState({socket})
-  
-      socket.on('number:change', (globalNumber) => {
-    this.setState({globalNumber})
-      })
-  
-    }
-  
-    onIncrement = () => this.state.socket.emit('increment')
-    onDecrement = () => this.state.socket.emit('decrement')
-    render(){
+class App extends Component {
+
+  state = { socket: null, globalNumber: 0 }
+
+  componentDidMount() {
+    const socket = io('http://localhost:8888');
+
+    this.setState({ socket })
+
+    socket.on('number:change', (globalNumber) => {
+      this.setState({ globalNumber })
+    })
+
+  }
+
+  onIncrement = () => this.state.socket.emit('increment')
+  onDecrement = () => this.state.socket.emit('decrement')
+  render() {
     // do something here to show the globalNumber and use increment and decrement
     return (
       <div>
-        hello
-        <onIncrement />
-        <onDecrement />
+        <div className="App">
+          <h1>{this.state.globalNumber}</h1>
+          <p className="App-intro">
+            <button onClick={this.onIncrement}>+</button>
+            <button onClick={this.onDecrement}>-</button>
+          </p>
+        </div>
       </div>
-)
-    }
+    )
   }
+}
 export default App;
